c440297fb853e1c761a5b4b181b11d0373204414
AdamSpin <adam.spinhirne@gmail.com>
2019-12-01 21:49:02 -0600

    Working on storing user data as JSON

--- app/src/main/java/com/example/myfinancepal/ui/home/CreateBudgetFragment.kt
+++ app/src/main/java/com/example/myfinancepal/ui/home/CreateBudgetFragment.kt
@@ -1,15 +1,31 @@
 package com.example.myfinancepal.ui.home
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myfinancepal.R
 import com.example.myfinancepal.R.layout.fragment_createbudget
+import kotlinx.android.synthetic.main.fragment_createbudget.*
 import kotlinx.android.synthetic.main.fragment_createbudget.view.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class CreateBudgetFragment : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(fragment_createbudget)
+
+        confirm_budget.setOnClickListener {
+            val budNew = JSONObject()
+            val budgetList = JSONArray()
+            budNew.put("name", field_budget_name.text)
+            budNew.put("amount", field_budget_amt.text)
+            budgetList.put(budNew)
+            val item = budgetList.getJSONObject(0)
+            val budName = item["name"]
+            val budAmt = item["amount"]
+            Toast.makeText(this, "Name: $budName Amount: $budAmt", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/myfinancepal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myfinancepal/ui/home/HomeFragment.kt
@@ -9,8 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myfinancepal.R
