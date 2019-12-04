cd1febeb03c819b10d9bdecb1ffddab527f10f74
AdamSpin <adam.spinhirne@gmail.com>
2019-12-03 21:50:20 -0600

    Working on dynamically displaying budgets on page

--- app/src/main/java/com/example/myfinancepal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myfinancepal/ui/home/HomeFragment.kt
@@ -1,20 +1,26 @@
 package com.example.myfinancepal.ui.home
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.d
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myfinancepal.R
 import com.example.myfinancepal.ui.dashboard.DashboardFragment
-import kotlinx.android.synthetic.main.fragment_budgets.budget_button
+import kotlinx.android.synthetic.main.fragment_budgets.*
 import kotlinx.android.synthetic.main.fragment_budgets.view.*
+import org.json.JSONArray
+import java.io.*
 
 class HomeFragment : Fragment() {
 
@@ -32,6 +38,30 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })*/
+        //Check if file exists. If not, fill it with an empty JSONArray
+//        var checkFile = File("budgetFile")
+//        if(!checkFile.exists()){
+//            val createFile : FileOutputStream = requireContext().openFileOutput("budgetFile", Context.MODE_PRIVATE)
+//            createFile.write("[]".toByteArray())
+//        }
+//        var fileInputStream: FileInputStream = requireContext().openFileInput("budgetFile")
+//        var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+//        val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+//        val stringBuilder: StringBuilder = StringBuilder()
+//        var text: String? = null
+//        while({text = bufferedReader.readLine(); text}() != null){
+//            stringBuilder.append(text)
+//        }
+//        var array = stringBuilder.toString()
+//        var budArray: JSONArray = JSONArray(array)
+
+//        for(i in 0 until budArray.length()){
+//            var bud = budArray.getJSONObject(i)
+//            var item = TextView(activity)
+//
+//        }
+        root.testText.text = "Changed"
+
         root.budget_button.setOnClickListener {
             val intent = Intent(activity, CreateBudgetFragment::class.java)
             requireContext().startActivity(intent)
