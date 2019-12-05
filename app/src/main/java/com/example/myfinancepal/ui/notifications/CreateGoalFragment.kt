b5378ff4a23d4ffb4690cb8b815948ab887ec92d
AdamSpin <adam.spinhirne@gmail.com>
2019-12-04 20:59:14 -0600

    Display Goals

--- app/src/main/java/com/example/myfinancepal/ui/notifications/CreateGoalFragment.kt
+++ app/src/main/java/com/example/myfinancepal/ui/notifications/CreateGoalFragment.kt
@@ -10,6 +10,7 @@ import androidx.core.view.marginTop
 import com.example.myfinancepal.MainActivity
 import com.example.myfinancepal.R
 import com.example.myfinancepal.R.layout.fragment_creategoal
+import kotlinx.android.synthetic.main.fragment_createbudget.*
 import kotlinx.android.synthetic.main.fragment_creategoal.*
 import kotlinx.android.synthetic.main.fragment_creategoal.view.*
 import org.json.JSONArray
@@ -22,30 +23,65 @@ class CreateGoalFragment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(fragment_creategoal)
 
-        //Check if file exists. If not, fill it with an empty JSONArray
-        var checkFile = File("goalFile")
-        if(!checkFile.exists()){
-            val tempDelete : FileOutputStream = openFileOutput("transactionFile", Context.MODE_PRIVATE)
-            tempDelete.write("[]".toByteArray())
+        var checkFile = applicationContext.getFileStreamPath("goalFile2")
+        if(checkFile == null || !checkFile.exists()){
+            var fileOutputStream : FileOutputStream = applicationContext.openFileOutput("goalFile2", Context.MODE_PRIVATE)
+            fileOutputStream.write("[]".toByteArray())
         }
-
-        //Read the file and create a JASONArray object from its contents
-        var fileInputStream: FileInputStream = openFileInput("goalFile")
+        var fileInputStream: FileInputStream? = null
+        fileInputStream = applicationContext.openFileInput("goalFile2")
         var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
         val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
         val stringBuilder: StringBuilder = StringBuilder()
         var text: String? = null
-        while({text = bufferedReader.readLine(); text}() != null){
+        while ({ text = bufferedReader.readLine(); text }() != null) {
             stringBuilder.append(text)
         }
-        var array = stringBuilder.toString()
-        var goalArray: JSONArray = JSONArray(array)
+        var fileString = stringBuilder.toString()
+        var goalArray = JSONArray(fileString)
 
         //When confirm_goal is pressed, error check and create new goal
         confirm_goal.setOnClickListener {
             var nameIsGood = true
-            var inputGoalName = field_goal_name.text.toString().trim()
-            var inputGoalAmt = field_goal_amt.text.toString().trim()
+            var itemName = field_goal_name.text.toString().trim()
+            var itemAmt = field_goal_amt.text.toString().trim()
+
+            if(itemName.isEmpty()){
+                Toast.makeText(this, "Goal must have a name", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                for(i in 0 until goalArray.length()){
+                    val goal = goalArray.getJSONObject(i)
+                    if(goal["name"] == itemName){
+                        nameIsGood = false
+                    }
+                }
+                if(!nameIsGood){
+                    Toast.makeText(this, "Goal already exists", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    if (itemAmt.isEmpty()) {
+                        Toast.makeText(this, "Goal must have an amount", Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (itemAmt.toFloat() < 0 || itemAmt.toFloat() > 100000) {
+                        Toast.makeText(this, "Goal amount out of range", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        var item = JSONObject()
+                        item.put("name", itemName)
+                        item.put("value", itemAmt)
+                        goalArray.put(item)
+
+                        var fileOutputStream: FileOutputStream =
+                            applicationContext.openFileOutput("goalFile2", Context.MODE_PRIVATE)
+                        fileOutputStream.write(goalArray.toString().toByteArray())
+
+                        Toast.makeText(this, "Goal Created", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
 
             //Check if user entered a budget name
         }
--- app/src/main/java/com/example/myfinancepal/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/myfinancepal/ui/notifications/NotificationsFragment.kt
@@ -1,12 +1,15 @@
 package com.example.myfinancepal.ui.notifications
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -16,6 +19,11 @@ import com.example.myfinancepal.ui.home.CreateBudgetFragment
 import com.example.myfinancepal.ui.home.CreateGoalFragment
 import kotlinx.android.synthetic.main.fragment_budgets.view.*
 import kotlinx.android.synthetic.main.fragment_goals.view.*
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
 
 class NotificationsFragment : Fragment() {
 
@@ -33,6 +41,64 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(this, Observer {
             textView.text = it
         })*/
+
+        var checkFile = requireActivity().applicationContext.getFileStreamPath("goalFile2")
+        if(checkFile == null || !checkFile.exists()){
+            var fileOutputStream : FileOutputStream = requireActivity().applicationContext.openFileOutput("goalFile2", Context.MODE_PRIVATE)
+            fileOutputStream.write("[]".toByteArray())
+        }
+
+        var fileInputStream: FileInputStream? = null
+        fileInputStream = requireActivity().applicationContext.openFileInput("goalFile2")
+        var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+        val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+        val stringBuilder: StringBuilder = StringBuilder()
+        var text: String? = null
+        while ({ text = bufferedReader.readLine(); text }() != null) {
+            stringBuilder.append(text)
+        }
+        val fileString = stringBuilder.toString()
+        val goalArray = JSONArray(fileString)
+        var lastId = root.goal_button.id
+
+        for(i in 0 until goalArray.length()){
+            val bud = goalArray.getJSONObject(i)
+            var item = TextView(activity)
+            val itemNameVal = bud["name"].toString() + ":"
+            item.text = itemNameVal
+            item.id = i
+            item.textSize = 25f
+            item.setPadding(15,0,0,0)
+            item.setTextColor(Color.rgb(0,51,76))
+            var params1 = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT)
+            params1.addRule(RelativeLayout.BELOW, lastId)
+            params1.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+            item.layoutParams = params1
+            root.goalLayout.addView(item)
+            lastId = i
+        }
+
+        root.button_goal_Clear.setOnClickListener {
+            var fileOutputStream : FileOutputStream = requireActivity().applicationContext.openFileOutput("goalFile2", Context.MODE_PRIVATE)
+            fileOutputStream.write("[]".toByteArray())
+        }
+
+        root.test_goal_button.setOnClickListener {
+            var ffileInputStream: FileInputStream? = null
+            ffileInputStream = requireActivity().applicationContext.openFileInput("goalFile2")
+            var iinputStreamReader: InputStreamReader = InputStreamReader(ffileInputStream)
+            val bbufferedReader: BufferedReader = BufferedReader(iinputStreamReader)
+            val sstringBuilder: StringBuilder = StringBuilder()
+            var ttext: String? = null
+            while ({ ttext = bbufferedReader.readLine(); ttext }() != null) {
+                sstringBuilder.append(ttext)
+            }
+            Log.d("Adam", sstringBuilder.toString())
+        }
+
+
         root.goal_button.setOnClickListener {
             Log.d("Adam", "Goal Button pressed")
             val intent = Intent(activity, CreateGoalFragment::class.java)
